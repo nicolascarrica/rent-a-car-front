19c69dad17d764cb143d8b8c2e3ca67a9cd8de1a
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2024-03-29 18:34:12 -0300

    Create app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useMemo } from "react"
+import styled, { ThemeProvider } from "styled-components"
+import { MyRoutes } from "./commons/routes/routes"
+import { BrowserRouter } from "react-router-dom"
+import { Sidebar } from "./commons/components/Sidebar/Sidebar"
+import { Light, Dark } from '../src/commons/styles/Themes'
 
-function App() {
-  const [count, setCount] = useState(0)
+// export const ThemeContext = React.createContext({})
+export const ThemeContext = React.createContext<{ theme: string; setTheme: React.Dispatch<React.SetStateAction<string>> } | null>(null)
+
+function App(): JSX.Element {
+  const [theme, setTheme] = React.useState("light")
+  const themeStyle = theme === "light" ? Light : Dark
+
+  const [sidebarOpen, setSidebarOpen] = React.useState(true)
+  const contextValue = useMemo(() => ({ theme, setTheme }), [theme, setTheme])
 
   return (
-    <>
-      <h1>Vite + React</h1>
-    </>
+  
+    <ThemeContext.Provider value={contextValue}>
+      <ThemeProvider theme={themeStyle}>
+        <BrowserRouter>
+          <Container className={sidebarOpen ? "sidebarState active" : "sidebarState"}> 
+            <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen}/>
+            <MyRoutes />    
+          </Container>
+        </BrowserRouter>
+      </ThemeProvider>
+    </ThemeContext.Provider>
+
   )
 }
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 90px auto;
+  background: ${({ theme }) => theme.bgtotal};
+  transition:all 0.3s;
+  &.active {
+    grid-template-columns: 300px auto;
+  }
+  color:${({theme})=>theme.text};
+`;
 
 export default App
