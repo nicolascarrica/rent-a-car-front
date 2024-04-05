412afc459f98e0d583acaaaa06bcf14a4e6b8e7b
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2024-04-05 11:59:28 -0300

    Update: Add Context providers

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ThemeContextProvider } from './commons/components/store/themeContext.tsx'
+import { SidebarContextProvider } from './commons/components/store/sidebarContext.tsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+  <ThemeContextProvider>
+        <SidebarContextProvider>
+          <App />
+        </SidebarContextProvider>
+      </ThemeContextProvider>
   </React.StrictMode>,
 )
  
