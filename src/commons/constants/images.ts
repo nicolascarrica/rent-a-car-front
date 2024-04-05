d4d5f6562247ed53e6e7863b9fd982cd912f4bb0
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2024-04-05 12:00:05 -0300

    Add images provider

---
+++ src/commons/constants/images.ts
@@ -0,0 +1,11 @@
+import logo from "../assets/images/rent-a-car.png";
+
+const images = {
+  logo: logo,
+  // avt: require("../assets/images/avatar.jpg"),
+  // logKey: require("../assets/images/Reset password-pana.svg"),
+  // dashboard: require("../assets/images/Revenue-cuate.svg"),
+  // notFound: require("../assets/images/Oops 404 Error with a broken robot-cuate.svg"),
+};
+
+export default images;
