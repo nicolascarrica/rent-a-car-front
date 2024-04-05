149548b68832310c7271cf000cef301d0f35ceb2
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2024-04-05 12:01:23 -0300

    Add sidebarNav elements

---
+++ src/commons/components/Config/sidebarNav.ts
@@ -0,0 +1,46 @@
+const sidebarNav = [
+  {
+    link: "/",
+    section: "dashboard",
+    icon: "lucide:layout-dashboard", //width:"20"
+    text: "Dashboard",
+  },
+  {
+    link: "/cars",
+    section: "cars",
+    icon: "icon-park-outline:car",
+    text: "Cars",
+  },
+  {
+    link: "/customers",
+    section: "customers",
+    icon: "ph:users-bold",
+    text: "Customers",
+  },
+  {
+    link: "/reservations",
+    section: "reservations",
+    icon: "icon-park-outline:transaction-order",
+    text: "Reservations",
+  },
+  // {
+  //   link: "/analytics",
+  //   section: "analytics",
+  //   icon: "carbon:analytics",
+  //   text: "Analytics",
+  // },
+  // {
+  //   link: "/discount",
+  //   section: "discount",
+  //   icon: "nimbus:discount-circle",
+  //   text: "Discount",
+  // },
+  // {
+  //   link: "/inventory",
+  //   section: "inventory",
+  //   icon: "ic:round-inventory",
+  //   text: "Inventory",
+  // },
+];
+
+export default sidebarNav;
