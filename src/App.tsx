709c464d988ef3c0333c83ee7e734df18fba8185
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2024-04-05 11:59:08 -0300

    Update: Delete unnecessary components

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,26 @@
-import React, { useMemo } from "react"
-import styled, { ThemeProvider } from "styled-components"
-import { MyRoutes } from "./commons/routes/routes"
-import { BrowserRouter } from "react-router-dom"
-import { Sidebar } from "./commons/components/Sidebar/Sidebar"
-import { Light, Dark } from '../src/commons/styles/Themes'
+import React from "react"
+import { BrowserRouter } from "react-router-dom";
+import Sidebar from "./commons/components/Sidebar/Sidebar";
+import '../src/commons/scss/App.scss'
+
+
 
 // export const ThemeContext = React.createContext({})
 export const ThemeContext = React.createContext<{ theme: string; setTheme: React.Dispatch<React.SetStateAction<string>> } | null>(null)
 
 function App(): JSX.Element {
-  const [theme, setTheme] = React.useState("light")
-  const themeStyle = theme === "light" ? Light : Dark
-
-  const [sidebarOpen, setSidebarOpen] = React.useState(true)
-  const contextValue = useMemo(() => ({ theme, setTheme }), [theme, setTheme])
-
-  return (
   
-    <ThemeContext.Provider value={contextValue}>
-      <ThemeProvider theme={themeStyle}>
+  return (
+      <div className="App">
         <BrowserRouter>
-          <Container className={sidebarOpen ? "sidebarState active" : "sidebarState"}> 
-            <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen}/>
-            <MyRoutes />    
-          </Container>
+            <Sidebar/> 
         </BrowserRouter>
-      </ThemeProvider>
-    </ThemeContext.Provider>
+      </div>
+        
 
   )
 }
 
-const Container = styled.div`
-  display: grid;
-  grid-template-columns: 90px auto;
-  background: ${({ theme }) => theme.bgtotal};
-  transition:all 0.3s;
-  &.active {
-    grid-template-columns: 300px auto;
-  }
-  color:${({theme})=>theme.text};
-`;
+
 
 export default App
