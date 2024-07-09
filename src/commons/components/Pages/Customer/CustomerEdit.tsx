8402010463d957cda7fd3c0dbf8ecd74d354e706
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2024-07-09 14:56:30 -0300

    Create customer edit page

---
+++ src/commons/components/Pages/Customer/CustomerEdit.tsx
@@ -0,0 +1,49 @@
+import { useParams } from "react-router-dom";
+import { ICustomersTable } from "../../../interfaces/Itable";
+import { customers } from "../../../constants/table";
+import LoadingSpinner from "../../UI/loadingSpinner/LoadingSpinner";
+import useFetch from "../../../hooks/useFetch";
+import EditCustomer from "../../Edit/EditCustomer/EditCustomer";
+const url = 'http://localhost:3000/api/v1/users'
+
+
+function CustomerEdit() {
+  const params = useParams();
+  const { userId } = params;
+
+
+   const customerInfo: ICustomersTable = customers.filter(
+     (item) => item.id.toString() === userId
+   )[0];
+
+   console.log(customerInfo)
+   let customerEdit;
+
+   const { data, error, status } = useFetch<ICustomersTable>(
+    `${url}/${userId}.json`
+   );
+   
+
+   if (status === "loading") {
+     customerEdit = <LoadingSpinner />;
+   }
+
+   if (error) {
+     customerEdit = <EditCustomer customer={customerInfo} />;
+   }
+
+   if (status === "fetched" && data) {
+     customerEdit = <EditCustomer customer={data} />;
+     console.log(data);
+   }
+
+   return (
+     <section>
+       <h2 className="title">Edit Customer</h2>
+       {customerEdit}
+     </section>
+   );
+  }
+
+
+export default CustomerEdit;
