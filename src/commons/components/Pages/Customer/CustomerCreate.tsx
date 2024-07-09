1d2292777bb0409489832ea8d174006c99f01e35
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2024-07-09 14:54:33 -0300

    Add page for create customers

---
+++ src/commons/components/Pages/Customer/CustomerCreate.tsx
@@ -0,0 +1,12 @@
+import CreateCustomer from "../../Create/CreateCustomer/CreateCustomer"
+
+function CustomerCreate() {
+  return(
+    <section>
+      <h2>Add Customer</h2>
+      <CreateCustomer/>
+    </section>
+  )
+}
+
+export default CustomerCreate
