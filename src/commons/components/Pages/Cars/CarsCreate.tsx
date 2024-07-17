6fd48388d24e600bc37e275f72d987cb597914ec
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2024-07-17 13:20:41 -0300

    Create Car pages

---
+++ src/commons/components/Pages/Cars/CarsCreate.tsx
@@ -0,0 +1,12 @@
+import CreateCars from "../../Create/CreateCars"
+
+const CarsCreate = () => {
+  return (
+    <section>
+      <h2> Add Cars </h2>
+      <CreateCars /> 
+    </section>
+  )
+}
+
+export default CarsCreate
