3f4fc96a49347ffb15f6a19b739254c8d1ba310e
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2024-07-17 13:24:34 -0300

    Create Select component

---
+++ src/commons/components/UI/input/Select.tsx
@@ -0,0 +1,65 @@
+import React, { useImperativeHandle, useRef, useState } from "react";
+import classes from "./Input.module.scss";
+
+interface SelectOption {
+  value: string;
+  label: string;
+}
+
+interface Props {
+  id: string;
+  options: SelectOption[];
+  classes?: string;
+  value?: string;
+  ref?: HTMLSelectElement;
+  onChange?: (event: React.FormEvent<HTMLSelectElement>) => void;
+}
+
+interface IImperativeHandler {
+  focus: () => void;
+  value?: string;
+}
+
+const Select = React.forwardRef<IImperativeHandler, Props>((props, ref) => {
+  const selectRef = useRef<HTMLSelectElement | null>(null);
+  const [value, setValue] = useState(props.value ?? "");
+
+  function selectChangeHandler(e: React.FormEvent<HTMLSelectElement>) {
+    setValue(e.currentTarget.value);
+    if (props.onChange) {
+      props.onChange(e);
+    }
+  }
+
+  function selectFocused() {
+    selectRef.current?.focus();
+    selectRef.current?.setAttribute("style", "border: 2px solid red");
+  }
+
+  useImperativeHandle(ref, () => {
+    return {
+      focus: selectFocused,
+      value: String(value),
+    };
+  });
+
+  return (
+    <div className={`${classes.form__control} ${props.classes}`}>
+      <label htmlFor={props.id}>{`${props.id}`}</label>
+      <select
+        ref={selectRef}
+        id={props.id}
+        value={value}
+        onChange={selectChangeHandler}
+      >
+        {props.options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+});
+
+export default Select;
