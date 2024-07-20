d6af425fa882c195685f339f96683c52f4b61d7d
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2024-07-20 12:15:31 -0300

    Add Reservation page

---
+++ src/commons/components/Pages/Reservation/ReservationCreate.tsx
@@ -0,0 +1,13 @@
+import CreateReservation from "../../Create/CreateReservation"
+
+
+const ReservationCreate = () => {
+  return (
+    <section>
+      <h2> Add Reservation </h2>
+      <CreateReservation /> 
+    </section>
+  )
+}
+
+export default ReservationCreate
