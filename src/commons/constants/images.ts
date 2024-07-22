687441556309e9dc46983144e81da6f55ce8fec6
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2024-07-22 09:38:08 -0300

    Add data and images

--- src/commons/constants/images.ts
+++ src/commons/constants/images.ts
@@ -1,11 +1,12 @@
 import logo from "../assets/images/rent-a-car.png";
 import avt from "../assets/images/avatar.jpg";
+import dashboard from "../assets/images/Revenue-cuate.svg"
 
 const images = {
   logo: logo,
   avt: avt,
   // logKey: require("../assets/images/Reset password-pana.svg"),
-  // dashboard: require("../assets/images/Revenue-cuate.svg"),
+  dashboard: dashboard,
   // notFound: require("../assets/images/Oops 404 Error with a broken robot-cuate.svg"),
 };
 
